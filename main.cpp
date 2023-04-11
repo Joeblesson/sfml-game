fe8c38e0b11685af508313dea3a92706b7a84721
Joeblesson <johanblesson@gmail.com>
2023-04-11 18:19:40 +0200

    whatever this is a screensaver now idc

--- main.cpp
+++ main.cpp
@@ -1,29 +1,24 @@
 #include <SFML/Graphics.hpp>
 
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML GAME");
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML GAME", sf::Style::Fullscreen);
     window.setFramerateLimit(60);
 
+    window.setMouseCursorVisible(false);
+
     sf::RectangleShape rect;
-
-    sf::Font Roboto;
-    Roboto.loadFromFile("fonts\\Roboto.ttf");
-
-    sf::Text myText;
-    myText.setFont(Roboto);
-    myText.setFillColor(sf::Color::Blue);
-    myText.setString("Press Esc to exit program");
-    myText.setPosition(300, 400);
-    myText.setCharacterSize(60);
+    int cc;
 
     sf::Vector2f rectanglePos(600, 350);
 
     rect.setPosition(rectanglePos);
 
-    rect.setSize(sf::Vector2f(100, 100));
+    rect.setSize(sf::Vector2f(150, 150));
+    rect.setFillColor(sf::Color::Red);
 
-    float xVelocity = 3;
-    float yVelocity = 3;
+    float xVelocity = 4;
+    float yVelocity = 4;
 
     while(window.isOpen()) {
         sf::Event event;
@@ -37,12 +32,49 @@ int main() {
         }
         //physics
         
-        if(rectanglePos.x < 0 || rectanglePos.x > 1180) {
+        if(rectanglePos.x < 0 || rectanglePos.x > 1770) {
             xVelocity *= -1;
+            cc += 1;
+            if(cc == 3) {
+                cc = 0;
+            }
+            switch(cc) {
+                case 0:
+                    rect.setFillColor(sf::Color::Red);
+                    break;
+                case 1:
+                    rect.setFillColor(sf::Color::Green);
+                    break;
+                case 2: 
+                    rect.setFillColor(sf::Color::Blue);
+                    break;
+                default:
+                    rect.setFillColor(sf::Color::Red);
+                    break;
+            }
+            
         }
 
-        if(rectanglePos.y < 0 || rectanglePos.y > 620) {
+        if(rectanglePos.y < 0 || rectanglePos.y > 930) {
             yVelocity *= -1;
+            cc++;
+            if(cc == 3) {
+                cc = 0;
+            }
+            switch(cc) {
+                case 0:
+                    rect.setFillColor(sf::Color::Red);
+                    break;
+                case 1:
+                    rect.setFillColor(sf::Color::Green);
+                    break;
+                case 2: 
+                    rect.setFillColor(sf::Color::Blue);
+                    break;
+                default:
+                    rect.setFillColor(sf::Color::Red);
+                    break;
+            }
         }
 
         rectanglePos.x += xVelocity;
@@ -52,10 +84,7 @@ int main() {
         // render 
 
         window.clear();
-        window.draw(myText);
         window.draw(rect);
         window.display();
     }
-
-    
 }
